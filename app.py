8884438449b0218a5544e047e677f29a453c6d7b
Elina Kim <elinakim@Ilviras-MacBook-Pro.local>
2022-12-26 14:59:35 -0800

    init

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+@app.route("/api/authors", methods = ['POST'])
+def create_author():
+    author = request.get_json()
+    
+    name = author['name']
+    
+    return name
+
+@app.route("/api/authors/<id>", methods = ['PUT'])
+def update_author(id):
+    author = request.json
+
+    name = author['name']
+
+    return name
+    
+
