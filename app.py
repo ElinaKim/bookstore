94a2a4197b58f54689e8b9354ab77b7aca2c2382
Elina Kim <elinakim791@gmail.com>
2023-01-08 20:21:43 -0800

    implement post, put, delete methods with database

--- app.py
+++ app.py
@@ -16,8 +16,12 @@ def create_author():
     author = request.get_json()
     
     name = author['name']
-    
-    return name
+
+    cur = conn.cursor()
+    cur.execute("INSERT INTO authors (name) VALUES (%s) RETURNING id",(name,))
+    records = cur.fetchall()
+
+    return {'authors': records}, 201
 
 @app.route("/api/authors/<id>", methods = ['PUT'])
 def update_author(id):
@@ -25,25 +29,45 @@ def update_author(id):
 
     name = author['name']
 
-    return name
+    cur = conn.cursor()
+    cur.execute("UPDATE authors SET name = (%s) WHERE id = (%s) RETURNING id", (name, id))
+    records = cur.fetchall()
+
+    return {'authors': records}, 200
+
+@app.route("/api/authors/<id>", methods = ['DELETE'])
+def delete_author(id):
+    cur = conn.cursor()
+    cur.execute("DELETE FROM authors WHERE id = (%s) RETURNING id",(id))
+    records = cur.fetchall()
+
+    return {'authors': records}, 202
     
 @app.route("/api/authors", methods = ['GET'])
 def get_all_authors():
     cur = conn.cursor()
     cur.execute("SELECT id, name FROM authors")
     records = cur.fetchall()
+
     return {'authors': records}, 200
 
 @app.route("/api/authors/<id>", methods = ['GET'])
 def get_author(id):
-    return id
+    cur = conn.cursor()
+    cur.execute("SELECT name FROM authors WHERE id = %s",(id))
+    records = cur.fetchone()
+
+    return {'authors': records}, 200
+
 
 @app.route("/api/books", methods = ["GET"])
 def get_all_books():
+
     return "All books"
 
 @app.route("/api/books/<id>", methods = ["GET"])
 def get_book(id):
+    
     return id
 
 @app.route("/api/books", methods = ["POST"])
