9886ac7cc984ab3d07bea529d4c05d89db2e7617
Elina Kim <elinakim791@gmail.com>
2023-01-09 18:13:00 -0800

    wrapped transactions with context manager "with"

--- app.py
+++ app.py
@@ -17,9 +17,10 @@ def create_author():
     
     name = author['name']
 
-    cur = conn.cursor()
-    cur.execute("INSERT INTO authors (name) VALUES (%s) RETURNING id",(name,))
-    records = cur.fetchall()
+    with conn:
+        with conn.cursor() as curs:
+            curs.execute("INSERT INTO authors (name) VALUES (%s) RETURNING id",(name,))
+            records = curs.fetchall()
 
     return {'authors': records}, 201
 
@@ -29,50 +30,56 @@ def update_author(id):
 
     name = author['name']
 
-    cur = conn.cursor()
-    cur.execute("UPDATE authors SET name = (%s) WHERE id = (%s) RETURNING id", (name, id))
-    records = cur.fetchall()
+    with conn:
+        with conn.cursor() as curs:
+            curs.execute("UPDATE authors SET name = (%s) WHERE id = (%s) RETURNING id", (name, id))
+            records = curs.fetchall()
 
     return {'authors': records}, 200
 
 @app.route("/api/authors/<id>", methods = ['DELETE'])
 def delete_author(id):
-    cur = conn.cursor()
-    cur.execute("DELETE FROM authors WHERE id = (%s) RETURNING id",(id))
-    records = cur.fetchall()
+    with conn:
+        with conn.cursor() as curs:
+            curs.execute("DELETE FROM authors WHERE id = (%s) RETURNING id",(id))
+            records = curs.fetchall()
 
     return {'authors': records}, 200
     
 @app.route("/api/authors", methods = ['GET'])
 def get_all_authors():
-    cur = conn.cursor()
-    cur.execute("SELECT id, name FROM authors")
-    records = cur.fetchall()
+    with conn:
+        with conn.cursor() as curs:
+            curs.execute("SELECT id, name FROM authors")
+            records = curs.fetchall()
 
     return {'authors': records}, 200
 
 @app.route("/api/authors/<id>", methods = ['GET'])
 def get_author(id):
-    cur = conn.cursor()
-    cur.execute("SELECT name FROM authors WHERE id = %s",(id))
-    records = cur.fetchone()
+    with conn:
+        with conn.cursor() as curs:
+            curs.execute("SELECT name FROM authors WHERE id = %s",(id))
+            records = curs.fetchone()
 
     return {'authors': records}, 200
 
 
 @app.route("/api/books", methods = ["GET"])
 def get_all_books():
-    cur = conn.cursor()
-    cur.execute("SELECT id, name FROM books")
-    records = cur.fetchall()
-    
+    with conn:
+        with conn.cursor() as curs:
+            curs.execute("SELECT id, name FROM books")
+            records = curs.fetchall()
+
     return {'books': records}, 200
 
 @app.route("/api/books/<id>", methods = ["GET"])
 def get_book(id):
-    cur = conn.cursor()
-    cur.execute("SELECT name FROM books WHERE id = %s",(id))
-    records = cur.fetchone()
+    with conn:
+        with conn.cursor() as curs:
+            curs.execute("SELECT name FROM books WHERE id = %s",(id))
+            records = curs.fetchone()
 
     return {'books': records}, 200
 
@@ -82,9 +89,10 @@ def create_book():
     
     name = book['name']
     
-    cur = conn.cursor()
-    cur.execute("INSERT INTO books (name) VALUES (%s) RETURNING id",(name,))
-    records = cur.fetchall()
+    with conn:
+        with conn.cursor() as curs:
+            curs.execute("INSERT INTO books (name) VALUES (%s) RETURNING id",(name,))
+            records = curs.fetchall()
 
     return {'books': records}, 201
 
@@ -94,16 +102,18 @@ def update_book(id):
 
     name = book['name']
 
-    cur = conn.cursor()
-    cur.execute("UPDATE books SET name = (%s) WHERE id = (%s) RETURNING id", (name, id))
-    records = cur.fetchall()
+    with conn:
+        with conn.cursor() as curs:
+            curs.execute("UPDATE books SET name = (%s) WHERE id = (%s) RETURNING id", (name, id))
+            records = curs.fetchall()
 
     return {'books': records}, 200
 
 @app.route("/api/books/<id>",methods = ["DELETE"])
 def delete_book(id):
-    cur = conn.cursor()
-    cur.execute("DELETE FROM books WHERE id = (%s) RETURNING id", (id))
-    records = cur.fetchall()
+    with conn:
+        with conn.cursor() as curs:
+            curs.execute("DELETE FROM books WHERE id = (%s) RETURNING id", (id))
+            records = curs.fetchall()
 
     return {'books': records}, 200
