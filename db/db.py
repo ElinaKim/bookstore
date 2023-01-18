521eeec4bc66f0d01dd073b973252e9695bcf4be
Leo Kim <e_leokim@yahoo.com>
2023-01-17 19:07:05 -0800

    Implement book&author repositories

---
+++ db/db.py
@@ -0,0 +1,38 @@
+import psycopg2
+from psycopg2 import Error
+
+class Db:
+    def __init__(self):
+        self.conn_str = "postgres://postgres:postgrespw@localhost:49153/bookstore"
+        self.conn = None
+        self.curs = None
+    
+    def connect(self):
+        try:
+            self.conn = psycopg2.connect(self.conn_str)
+        except Error as e:
+            print("Unable to connect to the database.")
+            print(e)
+        else:
+            print("Connection to the database was successful.")
+    
+    def __enter__(self):
+        print("entering context manager")
+        self.connect()
+        return self
+    
+    def __exit__(self, *args):
+        print("exiting context manager")
+        self.conn.close()
+
+    def query_all(self, query, params):
+        with self.conn:
+            with self.conn.cursor() as curs:
+                curs.execute(query, params)
+                return curs.fetchall()
+    
+    def query_one(self, query, params):
+        with self.conn:
+            with self.conn.cursor() as curs:
+                curs.execute(query, params)
+                return curs.fetchone()
